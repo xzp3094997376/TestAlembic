565281815d5a58f68ec06b2253bbeca9f8f2c12a
xzp <3094997376@qq.com>
2022-01-25 17:48:10 +0800

    init

---
+++ Assets/Test/TestTween.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestTween : MonoBehaviour
+{
+    public Text text;
+    public string content;
+    // Start is called before the first frame update    
+    void Start()
+    {
+        text.DOText(content, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
